9c645c538f548d6c7975e4d3255268f98c9fc535
CrisanFdez <crisanfdez@gmail.com>
2021-02-07 12:52:03 -0500

    fixed skeleton and css

--- src/components/Skeleton.js
+++ src/components/Skeleton.js
@@ -4,7 +4,15 @@ export const Skeleton = () => {
     return (
         <div className="skeleton">
             <div className="image"></div>
-            <p className="line"></p>
+            <p className="line temp"></p>
+            <p className="line description"></p>
+            <p className="line location"></p>
+            <p className="line btn-switch"></p>
+            <div className="row">
+                <p className="line"></p>
+                <p className="line"></p>
+                <p className="line"></p>
+            </div>
         </div>
     )
 }
